11144ce273130d243478e059fe7b520f22963f73
Alan Haber <dev@alanhaber.me>
2024-02-25 18:40:16 +0100

    feat: add sendEmail util

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,47 @@
+import nodemailer from "nodemailer";
+import path from "path";
+import fs from "fs";
+import Handlebars from "handlebars";
+
+const sendMail = async (
+  template: string,
+  contentItems: Object,
+  title: string,
+  email: string
+) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const templateFile = path.join(
+      __dirname,
+      `../../templates/${template}.hbs`
+    );
+    const templateContent: any = await fs.readFileSync(templateFile);
+
+    const templateCompiled = Handlebars.compile(templateContent.toString())(
+      contentItems
+    );
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: title,
+      html: templateCompiled,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) console.log(err);
+      else console.log("Email sent: " + info.response);
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export default sendMail;
