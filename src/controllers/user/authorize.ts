dddfd1af67625ee5669133cb93b876a1a8b011fd
Alan Haber <dev@alanhaber.me>
2024-09-24 01:01:51 +0200

    feat: authorize returns full user object

--- src/controllers/user/authorize.ts
+++ src/controllers/user/authorize.ts
@@ -5,7 +5,16 @@ export const authorize = async (
   req: Request & { user: IUser },
   res: Response
 ) => {
-  const { username } = req.user;
+  const user = req.user;
 
-  res.status(200).json({ username });
+  res.status(200).json({
+    _id: user.id,
+    avatar: Boolean(user.avatar),
+    firstName: user.firstName,
+    lastName: user.lastName,
+    username: user.username,
+    email: user.email,
+    passwordLastUpdated: user.passwordLastUpdated,
+    createdAt: user.createdAt,
+  });
 };
